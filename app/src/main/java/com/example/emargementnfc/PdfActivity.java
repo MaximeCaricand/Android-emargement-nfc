8b847be756e55bdd47de5d51bf29e617db0d2e50
Nicolas CRUVEILHER <385-cn172692@users.noreply.www-apps.univ-lehavre.fr>
2022-01-05 01:45:28 +0100

    Preview pdf

--- app/src/main/java/com/example/emargementnfc/PdfActivity.java
+++ app/src/main/java/com/example/emargementnfc/PdfActivity.java
@@ -6,40 +6,60 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.pdf.PdfDocument;
+import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.Manifest.permission.MANAGE_EXTERNAL_STORAGE;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.Inflater;
 
 public class PdfActivity extends AppCompatActivity {
 
-    Button generatePDFbtn;
     int pageHeight = 1120;
     int pagewidth = 792;
+    int currentPage = 0;
+
+    private PdfDocument pdfDocument;
+    private ImageView view_pdf;
+    private Button btn_generate;
+
+    private PdfRenderer renderer;
+    private PdfRenderer.Page rendererPage;
 
     private static final int PERMISSION_REQUEST_CODE = 200;
+    private static final String FILE_NAME = "monsuperpdf.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
 
-        generatePDFbtn = findViewById(R.id.pdf);
+        view_pdf = findViewById(R.id.view_pdf);
+        btn_generate = findViewById(R.id.pdf);
 
+        createPdf();
+        showPage(currentPage);
 
         if (checkPermission()) {
             Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
@@ -48,8 +68,8 @@ public class PdfActivity extends AppCompatActivity {
         }
     }
 
-    public void createPdf(View v) {
-        PdfDocument pdfDocument = new PdfDocument();
+    public void createPdf() {
+        pdfDocument = new PdfDocument();
         Paint myPaint = new Paint();
 
         PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
@@ -73,10 +93,72 @@ public class PdfActivity extends AppCompatActivity {
 
         pdfDocument.finishPage(myPage);
 
-        //File file = new File(Environment.getExternalStorageDirectory(), "GFG.pdf");
-        String myFilePath = Environment.getDataDirectory().getPath() + "/myPDFFile.pdf";
-        File file = new File(this.getFilesDir() + "/","sample.pdf");
-        File outDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString(), "monsuperpdf.pdf");
+        downloadPdf(getApplication().getCacheDir().getPath());
+    }
+
+    public void showPage(int index) {
+        try {
+            renderer = new PdfRenderer(ParcelFileDescriptor.open(
+                readFile(), ParcelFileDescriptor.MODE_READ_ONLY
+            ));
+            if (null != rendererPage) {
+                rendererPage.close();
+            }
+            rendererPage = renderer.openPage(index);
+
+            Bitmap bitmap = Bitmap.createBitmap(
+                rendererPage.getWidth(),
+                rendererPage.getHeight(),
+                Bitmap.Config.ARGB_4444
+            );
+
+            view_pdf.setImageBitmap(bitmap);
+            rendererPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            rendererPage.close();
+            renderer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void next(View v) {
+        currentPage = (currentPage + 1) % renderer.getPageCount();
+        showPage(currentPage);
+    }
+
+    public void previous(View v) {
+        currentPage = (currentPage - 1) % renderer.getPageCount();
+        showPage(currentPage);
+    }
+
+    private File readFile() {
+        File file = null;
+        try {
+            file = new File(getApplication().getCacheDir().getPath(), FILE_NAME);
+            FileInputStream is = new FileInputStream(file);
+
+            // Get the size of the file
+            long length = file.length();
+            byte[] bytes = new byte[(int) length];
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+                offset += numRead;
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+
+    public void downloadPdf(View v) {
+        downloadPdf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString());
+    }
+
+    public void downloadPdf(String path) {
+        File outDir = new File(path, FILE_NAME);
         try {
             pdfDocument.writeTo(new FileOutputStream(outDir));
             Toast.makeText(PdfActivity.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
